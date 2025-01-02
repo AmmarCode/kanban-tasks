c87b6ec9d8bf7b328f00c14e9bc42ecddbad20dc
Ammar <ammar.code01@gmail.com>
2025-01-01 23:18:03 -0500

    Add Boards, Columns, Tasks to Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeReducer from './themeSlice';
+import boardsReducer from './boardsSlice';
+import columnsReducer from './columnsSlice';
+import tasksReducer from './tasksSlice';
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
+    boards: boardsReducer,
+    columns: columnsReducer,
+    tasks: tasksReducer,
   },
 });
